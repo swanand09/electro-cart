d491b778d9fe874da409a40df75f3495c6ad4c8b
swanand reddy <swanand.l@valnetinc.com>
2021-03-26 09:29:15 -0400

    add views

--- public/index.php
+++ public/index.php
@@ -5,14 +5,32 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+// use Slim\Views\Twig;
+// use Slim\Http\Uri;
+// use Slim\Http\Environment;
+
 use Tracktik\Controller\PurchaseItem as Controller_PurchaseItem;
 
 $app = AppFactory::create();
 
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+// $container['view'] = function ($container) {
+//     $view = new Twig(__DIR__ .'/../src/view', [
+//         'cache' => __DIR__ .'../var/cache/twig'
+//     ]);
+
+    
+//     return $view;
+// };
+
 $app->addErrorMiddleware(true, false, false);
 
-$app->get('/', Controller_PurchaseItem::class . ':listAllItems');
+$app->get('/api/list-items-purchase/', Controller_PurchaseItem::class . ':apiGetItems');
 
-
+$app->get('/', Controller_PurchaseItem::class . ':getItems');
 
 $app->run();
--- src/controller/PurchaseItem.php
+++ src/controller/PurchaseItem.php
@@ -11,7 +11,7 @@ class PurchaseItem
 {
 
 
-    private function execute($itemsBought,Response $response) :Response
+    private function renderJson($itemsBought,Response $response) :Response
 	{
 		try{
 			
@@ -32,7 +32,28 @@ class PurchaseItem
 		}
 	}
 
-    public function listAllItems(Request $request, Response $response) :Response
+    private function renderHtml($itemsBought,Response $response) :Response
+	{
+		try{
+			
+			if(isset($itemsBought['error'])){
+				throw new \ErrorException($itemsBought['error']);
+			}
+			
+			$response->getBody()->write(json_encode($itemsBought,JSON_PRETTY_PRINT));
+			return $response
+				->withHeader('Content-Type', 'application/json')
+				->withHeader('Access-Control-Allow-Origin', '*')
+				->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+				->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+		} catch(\ErrorException $e){
+			$response->getBody()->write(json_encode(["error"=>["text"=>$e->getMessage()]]));
+			return $response
+				->withHeader('Content-Type', 'application/json');
+		}
+	}
+
+    public function apiGetItems(Request $request, Response $response) :Response
     {
         $televisionOne = Purchase::television(202.54);
         $televisionOne->setExtras(2);
@@ -58,7 +79,39 @@ class PurchaseItem
 
         $sortedItems = Purchase::getSortedItems($items, 'price');
         $totalPrice = Purchase::getTotalPrice($sortedItems);
-        return $this->execute(compact([
+        return $this->renderJson(compact([
+                                "sortedItems","totalPrice"
+                              ]),$response);
+    }
+
+
+    public function getItems(Request $request, Response $response) :Response
+    {
+        $televisionOne = Purchase::television(202.54);
+        $televisionOne->setExtras(2);
+        $televisionOne->setListExtras(Purchase::controller('remote'));
+        $televisionOne->setListExtras(Purchase::controller('remote`'));
+
+        $televisionTwo = Purchase::television(430.95);
+        $televisionTwo->setExtras(1);
+        $televisionTwo->setListExtras(Purchase::controller('remote'));
+
+        $console = Purchase::console(200.45);
+        $console->setListExtras(Purchase::controller('remote'));
+        $console->setListExtras(Purchase::controller('remote'));
+        $console->setListExtras(Purchase::controller('wired'));
+        $console->setListExtras(Purchase::controller('wired'));
+
+        $items = [
+                    $televisionOne,
+                    $televisionTwo,
+                    $console,
+                    Purchase::microwave(123.99),
+                ];
+
+        $sortedItems = Purchase::getSortedItems($items, 'price');
+        $totalPrice = Purchase::getTotalPrice($sortedItems);
+        return $this->renderHtml(compact([
                                 "sortedItems","totalPrice"
                               ]),$response);
     }
